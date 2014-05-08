69f6cad0ec5fb9f27ecfe250a6cceea20273c02e
Thomas Leen <tl@thomasleen.com>
2014-05-08 01:06:26 +0000

    fished file config

---
+++ src/jade/include/expanding.js
@@ -0,0 +1,7 @@
+jade: {
+  options: {
+    pretty: false,
+//...
+  },
+  templates: {expand: true , cwd: 'src/jade', src: ['*.jade'], dest: 'tmp/', ext: '.html'}
+}
---
+++ src/jade/include/globbing.js
@@ -0,0 +1,4 @@
+aws_s3: {
+//...
+  upload: {action: 'upload', expand: true, cwd: 'dist/', src: ['**/**.*','!README.md'], dest: ''}      
+},
